da440ba17bbae1549f2b5cdaf4f8085f2719db52
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-06-15 11:27:27 +0100

    coin select component file
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

---
+++ src/components/trade/coin-popup.tsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+
+// styling
+import './trade.css';
+
+export default  function CoinPopup({popupStateOne, popupStateTwo, OneInput, TwoInput} :
+{popupStateOne:boolean, popupStateTwo:boolean, OneInput:string, TwoInput:string}) {
+
+  return (
+    <div className="coin-popup-wrapper">
+      <div className="coin-popup-container">
+
+      </div>
+    </div>
+  )
+}
