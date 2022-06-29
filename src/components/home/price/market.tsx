0006814020625201a0de43ade44327562ba1c65a
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-06-29 21:50:41 +0100

    price market updating animation
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/components/home/price/market.tsx
+++ src/components/home/price/market.tsx
@@ -20,7 +20,11 @@ type coinData = {
 export default function Market() {
   const [btcData, setBtcData] = useState<coinData>({price: 0, sparkline: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], change: 0});
   const [ethData, setEthData] = useState<coinData>({price: 0, sparkline: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], change: 0});
+  const [ethStatus, setEthStatus] = useState<boolean>(false);
+  const [btcStatus, setBtcStatus] = useState<boolean>(false);
   useEffect(() =>{
+    setBtcStatus(true);
+    setEthStatus(true);
     const getPrices = async () => {
      const getBtcPrice = await btcPrice()
        .then((result:any) => {
@@ -30,6 +34,7 @@ export default function Market() {
 	   change: result.market_data.price_change_percentage_7d
          }
          setBtcData(marketObj); 
+	 setBtcStatus(false);
        })
        .catch(() => {
          console.log("failed to get btc data");
@@ -43,6 +48,7 @@ export default function Market() {
 	   change: result.market_data.price_change_percentage_7d
          }
          setEthData(marketObj);
+	 setEthStatus(false);
        })
        .catch(() => {
          console.log("failed to get eth data"); 
@@ -69,6 +75,18 @@ export default function Market() {
             <p className="market-component-prices-name">BTC - USD</p>
 	    <div className="market-component-price-update-status-container">
 	      <p className="market-component-price">${(btcData.price).toLocaleString()}</p>
+        {btcStatus === true && (
+	  <>
+            <p className="price-component-update-status">updating price</p> 
+	    <svg className="price-update-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="auto" viewBox="0 0 30 30" style={{fill: "rgb(216, 216, 216)"}}><path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"></path></svg>
+	  </>
+	)}
+        {btcStatus === false && (
+          <>
+	  <div className="block-status-active"></div>
+	  </>
+        )}
+
 	    </div>
 	  </div>
 	  <p className="market-component-price-full-name">Bitcoin</p>
@@ -90,6 +108,18 @@ export default function Market() {
             <p className="market-component-prices-name">ETH - USD</p>
 	    <div className="market-component-price-update-status-container">
 	      <p className="market-component-price">${(ethData.price).toLocaleString()}</p>
+        {ethStatus === true && (
+	  <>
+            <p className="price-component-update-status">updating price</p> 
+	    <svg className="price-update-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="auto" viewBox="0 0 30 30" style={{fill: "rgb(216, 216, 216)"}}><path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"></path></svg>
+	  </>
+	)}
+        {ethStatus === false && (
+          <>
+	  <div className="block-status-active"></div>
+	  </>
+        )}
+
 	    </div>
 	  </div>
 	  <p className="market-component-price-full-name">Ethereum</p>
